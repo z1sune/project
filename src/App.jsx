fd675e85d289e4ddcc2597e91a1bd818513a5b37
jisun <3unzie@naver.com>
2023-04-24 10:48:47 +0900

    :construction: app 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+import CursorProvider from 'utils/CursorProvider';
+
+import Home from 'components/Home/Home';
+import Footer from 'components/Footer/Footer';
+import Aim from 'components/Aim/Aim';
+
+import Info from 'pages/Info/Info';
+import Project from 'pages/Project/Project';
+import Contact from 'pages/Contact/Contact';
+
+import 'styles/Global.scss';
+
+const App = () => {
+  const [cursor, setCursor] = useState(false);
+  const [link, setLink] = useState('info');
+
+  const getCursorState = val => setCursor(val);
+  const getLink = val => setLink(val);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CursorProvider cursor={cursor} />
+      <main className="main">
+        <Aim link={link} getCursorState={getCursorState} />
+        <Routes>
+          <Route path="/" element={<Home getLink={getLink} getCursorState={getCursorState} />} />
+          <Route path="info" element={<Info getLink={getLink} />} />
+          <Route path="project" element={<Project getLink={getLink} getCursorState={getCursorState} />} />
+          <Route path="contact" element={<Contact getLink={getLink} />} />
+        </Routes>
+      </main>
+      <Footer getCursorState={getCursorState} />
+    </>
   );
-}
+};
 
 export default App;
